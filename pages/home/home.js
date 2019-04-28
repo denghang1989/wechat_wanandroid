586d7c414d7849501857aa14eea1426897e4cca0
denghang1989 <denghang1989@163.com>
2019-04-28 18:25:13 +0800

    第一次提交

---
+++ pages/home/home.js
@@ -0,0 +1,92 @@
+// pages/home/home.js
+var appInst = getApp();
+var baseUrl = appInst.globalData.baseUrl;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
+    imgUrls: [],
+    pageNum:0,
+    itemList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url:  baseUrl+ '/banner/json',
+      method: 'GET',
+      success: (result) => {
+        this.setData({
+          imgUrls: result.data.data
+        })
+      }
+    });
+
+    wx.request({
+      url: baseUrl+`article/list/${this.data.pageNum}/json`,
+      method: 'GET',
+      success: (result)=>{
+        this.setData({
+          itemList:result.data.data
+        })
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
